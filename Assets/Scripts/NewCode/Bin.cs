d515e3d289f4296db17ec18327b650382e60f4b9
Ebakryak <75696359+DaniilUnire@users.noreply.github.com>
2022-08-21 09:50:01 +0300

    21.08.2022

--- Assets/Scripts/NewCode/Bin.cs
+++ Assets/Scripts/NewCode/Bin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Bin : MonoBehaviour
 {
     public int Acceptable;
---
+++ Assets/Scripts/NewCode/VerticalDropdown.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UntyEngine.UI;
+
+public class VerticalDropdown : MonoBehaviour
+{
+    
+    public Text 
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+
+    public void Swipe (int Direction)
+    {
+
+    }
+
+}
