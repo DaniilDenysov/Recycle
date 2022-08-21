0843428b1f7f3e2e4fe17f21472222ba401551bd
Ebakryak <75696359+DaniilUnire@users.noreply.github.com>
2022-08-21 12:53:29 +0300

    21.08.2020(VerticalDropdown fix)

--- Assets/Scripts/NewCode/VerticalDropdown.cs
+++ Assets/Scripts/NewCode/VerticalDropdown.cs
@@ -1,27 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UntyEngine.UI;
+using UnityEngine.UI;
+
 
 public class VerticalDropdown : MonoBehaviour
 {
-    
-    public Text 
 
-    void Start()
-    {
-        
-    }
-
-  
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Text output;
+    [SerializeField] private string[] Option;
 
     public void Swipe (int Direction)
     {
-
+        if (Direction > 0)
+        {
+            for (int i = 0; i < Option.Length - 1; i++)
+            {
+                if (output.text == Option[i])
+                {
+                    if (i + 1 > Option.Length - 1)
+                    {
+                        output.text = Option[0];
+                        break;
+                    } else
+                    {
+                        output.text = Option[i];
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = Option.Length - 1; i > 0; i--)
+            {
+                if (output.text == Option[i])
+                {
+                    if (i - 1 < 0)
+                    {
+                        output.text = Option[Option.Length - 1];
+                        break;
+                    }
+                    else
+                    {
+                        output.text = Option[i-1];
+                        break;
+                    }
+                }
+            }
+        }
     }
 
 }
