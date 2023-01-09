871f1a34aa5ab63ce7d97cb3ccbfef7fc5f45486
DaniilUnire <75696359+DaniilUnire@users.noreply.github.com>
2023-01-09 23:18:31 +0200

    09.01.2023

--- Assets/Scripts/SpawnTrophey.cs
+++ Assets/Scripts/SpawnTrophey.cs
@@ -7,20 +7,23 @@ using UnityEngine;
 public class SpawnTrophey : MonoBehaviour
 {
     public BoxCollider2D spawnRange;
-    private System.Random random;
+    private System.Random random = new System.Random();
+
+    private float totalProbability = 0f;
 
     [System.Serializable]
     public class Trophey
     {
         public GameObject prefab;
         [Range(0, 100f)] public float probability = 50f;
+        public double weight;
     }
 
-    [SerializeField] private List<Trophey> tropheys;
+    [SerializeField] private Trophey [] tropheys;
 
     private void LateUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             spawnTrophey();
         }
@@ -30,19 +33,31 @@ public class SpawnTrophey : MonoBehaviour
     {
         Instantiate(tropheys[getIndex()].prefab,new Vector3(Random.Range(spawnRange.size.x / 2, -spawnRange.size.x / 2),transform.position.y, 2),Quaternion.identity);
     }
-    public float calculateProbability()
+    public void calculateProbability()
     {
-        float newProbability = 0f;
-        for (int i = 0;i<tropheys.Count;i++)
-            newProbability += tropheys[i].probability;
 
-        return newProbability;
+        totalProbability = 0f;
+        foreach (Trophey trophey in tropheys)
+        {
+            totalProbability += trophey.probability;
+            trophey.weight = totalProbability;
+        }
+        /* float newProbability = 0f;
+         Debug.Log("Count: " + tropheys.Count);
+         for (int i = 0;i<tropheys.Count;i++)
+             newProbability += tropheys[i].probability;
+
+         Debug.Log("Prob: " + newProbability);
+         return newProbability;*/
     }
     public int getIndex ()
     {
-        double rand = random.NextDouble() * calculateProbability();
-        for (int i = 0;i<tropheys.Count;i++)
-            if (rand <= tropheys[i].probability) return i;
+        calculateProbability();
+        double rand = random.NextDouble() * totalProbability;
+        int localIndex = 0;
+        Debug.Log("Rand: " + rand);
+        for (int i = 0; i < tropheys.Length; i++)
+            if (rand <= tropheys[i].weight) return i;
 
         return 0;
     }
