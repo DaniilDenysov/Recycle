3176b09ce03f82d7fe7159122c84b435236b3165
DaniilUnire <75696359+DaniilUnire@users.noreply.github.com>
2023-01-11 11:13:09 +0200

    11.01.2023

--- Assets/Scripts/SpawnTrophey.cs
+++ Assets/Scripts/SpawnTrophey.cs
@@ -9,6 +9,8 @@ public class SpawnTrophey : MonoBehaviour
     public BoxCollider2D spawnRange;
     private System.Random random = new System.Random();
 
+    [Range(0, 100f)] public float spawnProbability = 50f;
+
     private float totalProbability = 0f;
 
     [System.Serializable]
@@ -21,6 +23,11 @@ public class SpawnTrophey : MonoBehaviour
 
     [SerializeField] private Trophey [] tropheys;
 
+    private void Start()
+    {
+        InvokeRepeating(nameof(spawnTrophey),10,10);
+    }
+
     private void LateUpdate()
     {
         if (Input.GetKeyUp(KeyCode.Space))
@@ -31,7 +38,9 @@ public class SpawnTrophey : MonoBehaviour
 
     public void spawnTrophey ()
     {
-        Instantiate(tropheys[getIndex()].prefab,new Vector3(Random.Range(spawnRange.size.x / 2, -spawnRange.size.x / 2),transform.position.y, 2),Quaternion.identity);
+        float rand = Random.Range(0, 101);
+        Debug.Log("Spawn: " + rand);
+        if (rand <= spawnProbability) Instantiate(tropheys[getIndex()].prefab,new Vector3(Random.Range(spawnRange.size.x / 2, -spawnRange.size.x / 2),transform.position.y, 2),Quaternion.identity);
     }
     public void calculateProbability()
     {
@@ -42,13 +51,6 @@ public class SpawnTrophey : MonoBehaviour
             totalProbability += trophey.probability;
             trophey.weight = totalProbability;
         }
-        /* float newProbability = 0f;
-         Debug.Log("Count: " + tropheys.Count);
-         for (int i = 0;i<tropheys.Count;i++)
-             newProbability += tropheys[i].probability;
-
-         Debug.Log("Prob: " + newProbability);
-         return newProbability;*/
     }
     public int getIndex ()
     {
