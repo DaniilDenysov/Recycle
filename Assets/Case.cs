70d3b223cd61167b5d6df5bd50b106ee7d0c4928
DaniilUnire <75696359+DaniilUnire@users.noreply.github.com>
2023-03-05 00:28:43 +0200

    05.03.2023

--- Assets/Case.cs
+++ Assets/Case.cs
@@ -4,17 +4,53 @@ using UnityEngine;
 
 public class Case : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem confety;
+    [SerializeField] private ParticleSystem confety,pieces;
     [SerializeField] private Animator trophey_animator,case_animator;
+
+    private enum State
+    {
+        Open_1,
+        Open_2,
+        Open_3,
+        Open_finish,
+        Take
+    }
+
+    private State state;
+
 
     public void PlayConfety ()
     {
         confety.Play();
+        pieces.Play();
     }
 
     public void openCase ()
     {
-        case_animator.SetTrigger("Open");
+        switch (state)
+        {
+            case State.Open_1:
+                state = State.Open_2;
+               case_animator.SetBool("Open", true);
+                break;
+            case State.Open_2:
+                state = State.Open_3;
+                case_animator.SetTrigger("Next");
+                break;
+            case State.Open_3:
+                state = State.Open_finish;
+                case_animator.SetTrigger("Next");
+                break;
+            case State.Open_finish:
+                state = State.Take;
+                case_animator.SetTrigger("Next");
+                break;
+            case State.Take:
+                state = State.Open_1;
+                takeTrophey();
+                case_animator.SetBool("Open", false);
+                break;
+        }
     }
 
     public void takeTrophey ()
